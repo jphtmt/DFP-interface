13b5f3d5f64822c55c786ccb7a497a350baeddd7
jiph <jiph@bsfit.com.cn>
2017-08-29 16:49:07 +0800

    pytest改写

--- test_web_ui.py
+++ test_web_ui.py
@@ -22,6 +22,49 @@ def base_capbility(request):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    # if ua:
+    #     pass
+    # else:
+    #     chrome_options.add_argument('user-agent="asdfwevcsdgeft"')
+    # if language != 'zh-cn':
+    #     chrome_options.add_argument('lang=en-US')
+    # else:
+    #     chrome_options.add_argument('lang=zh-cn')
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path=r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
+    def fin():
+        print "browser close"
+        driver.quit()
+
+    request.addfinalizer(fin)
+    return driver
+
+
+@pytest.fixture()
+def base_capbility_ua(request):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('user-agent="asdfwevcsdgeft"')
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path=r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
+    def fin():
+        print "browser close"
+        driver.quit()
+
+    request.addfinalizer(fin)
+    return driver
+
+
+@pytest.fixture()
+def base_capbility_proxy(request):
+    chrome_options = webdriver.ChromeOptions()
+    proxy = "192.168.12.36:8888"
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--proxy-server=%s' % proxy)
     driver = webdriver.Chrome(chrome_options=chrome_options,
                               executable_path=r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
 
@@ -46,24 +89,79 @@ def get_mongo_data(clean=True):
     return mongo_data
 
 
+@pytest.fixture()
+def base_capbility_language(request):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--lang=zh-cn')
+
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path=r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
+    def fin():
+        print "browser close"
+        driver.quit()
+
+    request.addfinalizer(fin)
+    return driver
+
+
+@pytest.fixture()
+def base_capbility_plugins(request):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-extensions')
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path=r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
+    def fin():
+        print "browser close"
+        driver.quit()
+
+    request.addfinalizer(fin)
+    return driver
+
+
+@pytest.mark.skip()
 def test_get_normall(base_capbility):
     driver = base_capbility
     url = "http://10.100.1.53:9080/public/index.html"
     driver.get(url)
     # driver.set_window_size(480,800)
-    WebDriverWait(driver, 3).until(
+    WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
     time.sleep(3)
     assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data()['dfp']
     driver.delete_all_cookies()
 
 
+@pytest.fixture()
+def base_capbility_incognito(request):
+    chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--incognito')
+
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path=r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+
+    def fin():
+        print "browser close"
+        driver.quit()
+
+    request.addfinalizer(fin)
+    return driver
+
+
+@pytest.mark.skip()
 def test_delete_cookie(base_capbility):
     driver = base_capbility
     url = "http://10.100.1.53:9080/public/index.html"
     driver.get(url)
     # driver.set_window_size(480,800)
-    WebDriverWait(driver, 3).until(
+    WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
     assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
     driver.delete_all_cookies()
@@ -72,34 +170,141 @@ def test_delete_cookie(base_capbility):
     assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data()['dfp']
 
 
+@pytest.mark.skip()
 def test_timezone(base_capbility):
     driver = base_capbility
     url = "http://10.100.1.53:9080/public/index.html"
     driver.get(url)
     # driver.set_window_size(480,800)
-    WebDriverWait(driver, 3).until(
+    WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
     assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
     driver.delete_all_cookies()
     call_powershell_timezone()
     driver.refresh()
-    WebDriverWait(driver, 3).until(
+    WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
     assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data()['dfp']
     call_powershell_timezone('-TimeZoneFriendlyName "China Standard Time"')
 
 
+@pytest.mark.skip()
 def test_screnn_size(base_capbility):
     driver = base_capbility
     url = "http://10.100.1.53:9080/public/index.html"
     driver.maximize_window()
     driver.get(url)
-    WebDriverWait(driver, 3).until(
+    WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
     assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
     driver.delete_all_cookies()
     driver.set_window_size(480, 800)
     driver.refresh()
-    WebDriverWait(driver, 3).until(
+    WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
     assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data()['dfp']
+
+
+@pytest.mark.skip()
+def test_ua(base_capbility, base_capbility_ua):
+    driver = base_capbility
+    url = "http://10.100.1.53:9080/public/index.html"
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
+    driver.delete_all_cookies()
+    driver = base_capbility_ua
+    url = 'http://10.100.1.53:9080/public/index.html'
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data()['dfp']
+
+
+@pytest.mark.skip()
+def test_language(base_capbility, base_capbility_language):
+    driver = base_capbility
+    url = "http://10.100.1.53:9080/public/index.html"
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
+    driver.delete_all_cookies()
+    driver = base_capbility_language
+    url = 'http://10.100.1.53:9080/public/index.html'
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data()['dfp']
+
+
+@pytest.mark.skip()
+def test_incognito(base_capbility, base_capbility_incognito):
+    driver = base_capbility
+    url = "http://10.100.1.53:9080/public/index.html"
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
+    driver.delete_all_cookies()
+    driver = base_capbility_incognito
+    url = 'http://10.100.1.53:9080/public/index.html'
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data()['dfp']
+
+
+@pytest.mark.skip()
+def test_proxy(base_capbility, base_capbility_proxy):
+    driver = base_capbility
+    url = "http://10.100.1.53:9080/public/index.html"
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
+    driver.delete_all_cookies()
+    driver = base_capbility_proxy
+    url = 'http://10.100.1.53:9080/public/index.html'
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data()['dfp']
+
+
+# @pytest.mark.skip()
+def test_no_plugins(base_capbility, base_capbility_plugins):
+    driver = base_capbility
+    url = "http://10.100.1.53:9080/public/index.html"
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
+    driver.delete_all_cookies()
+    driver = base_capbility_plugins
+    url = 'http://10.100.1.53:9080/public/index.html'
+    driver.maximize_window()
+    driver.get(url)
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="inner"]')))
+    print driver.find_element_by_xpath('//*[@id="inner"]').text
+    assert driver.find_element_by_xpath('//*[@id="inner"]').text == get_mongo_data(clean=False)['dfp']
